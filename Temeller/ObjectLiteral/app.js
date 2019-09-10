c235412a7211dad1f246ffdf04b72450ba88f12e
abdullahnur.ybs@gmail.com <abdullahnur.ybs@gmail.com>
2019-09-10 15:42:02 +0300

    dene

---
+++ Temeller/ObjectLiteral/app.js
@@ -0,0 +1,27 @@
+let value;
+const programer = {
+    name : "abdullah nur",
+    age : 20,
+    email : "abduşşah@gmail.com",
+    langs: ["python,java,c++"],
+
+    address : {
+        city : "mersin",
+        street:"menteş",
+
+
+
+    },
+
+    work: function(){
+
+        console.log("programcı çalışıyır ...");
+
+
+    }
+
+}
+
+value = programer;
+
+console.log(value);
