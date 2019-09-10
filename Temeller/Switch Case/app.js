fa97917846b89c316a1fe6999bb9e03fda03cd1d
abdullahnur.ybs@gmail.com <abdullahnur.ybs@gmail.com>
2019-09-10 18:49:57 +0300

    commit

---
+++ Temeller/Switch Case/app.js
@@ -0,0 +1,24 @@
+const proces = 2;
+
+switch(proces){
+    case 1 :
+        console.log( "işlem 1 ");
+        break;
+    case 2 :
+        console.log( "işlem 2 ");
+        break;
+    case 3 :
+        console.log( "işlem 3 ");
+        break;
+    
+        default:
+            console.log("geçersiz işlem ");
+
+
+
+
+
+
+
+
+}
