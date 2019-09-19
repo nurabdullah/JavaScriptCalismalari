9d47ecc30f11d16686aaa057498193b9f0c80086
abdullahnur.ybs@gmail.com <abdullahnur.ybs@gmail.com>
2019-09-19 17:46:09 +0300

    dom eventleri

--- Temeller/TODOLIST/app.js
+++ Temeller/TODOLIST/app.js
@@ -1,46 +1,91 @@
-// window objesi
 
-let value ;
+//Elementi Id e göre seçme 
 
- value=document;
-value=document.all;
-value=document.all.length;
-value=document.all[0];
-value=document.all[6];
-value=document.all[document.all.length-1];
+let element;
 
+element = document.getElementById("todo-form");
 
-const element = document.all;
-for(let i = 0 ; i < element.length ;i++){
+element = document.getElementById("tasks-title");
 
-console.log(element[i]);
-
-}
+// element class a göre seçme 
 
 
-value=document.scripts; 
+element= document.getElementsByClassName("list-group-item");
 
-value=document.scripts.length;
-value=document.scripts[0];
-
-// Linkler
-
-value =document.links;
-value=document.links[0];
-value=document.links[document.links.length-1];
-value=document.links[document.links.length-1].getAttribute("class");
-value=document.links[document.links.length-1].getAttribute("href");
-value=document.links[document.links.length-1].className;
-value=document.links[document.links.length-1].classList;
-
-
-//formlar
-
-value = document.forms;
-value=document.forms.length;
-value=document.forms[0].id;
-value=document.forms[0].method;
+element = document.getElementsByClassName("card-header");
 
 
 
-console.log(value);
+// elementi taga göre seçme 
+
+element = document.getElementsByTagName("li");
+
+
+
+
+//  query selector - css selector 
+
+element =document.querySelector("#todo-form");
+element = document.querySelector("#tasks-title");
+
+element= document.querySelector(".list-group-item");
+
+//quersyselectorall - tüm elemetnleri seçme 
+
+element= document.querySelectorAll(".list-group-item");
+
+
+console.log( element);
+
+
+
+yeni element oluşturma 
+
+const newlink = document.createElement("a");
+
+const cardbody = document.getElementsByClassName("card-body")[1];
+
+
+
+newlink.id = "clear-todos";
+newlink.className = "btn btn-danger";
+
+newlink.href= "https://www.google.com.tr";
+
+newlink.target = "_blank";
+
+
+
+newlink.appendChild(document.createTextNode("farklı bir sayfaya git"));
+
+cardbody.appendChild(newlink);
+
+
+console.log(newlink);
+
+
+
+dinamik element silme 
+
+const todolist = document.querySelector("ul.list-group");
+
+const todos = document.querySelectorAll("li.list-group-item")
+ 
+
+
+todos[0].remove();
+
+todolist.removeChild(todos[2]);
+
+console.log(todos);
+console.log(todolist);
+
+
+const filterInput = document.getElementById("filter");
+
+filterInput.addEventListener("focus",function(e){
+
+    console.log("naber"); 
+
+});
+
