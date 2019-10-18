809299c051318271ddec4f8a5c3614c047212b23
nurabdullah <abdullahnur.ybs@gmail.com>
2019-10-19 00:42:07 +0300

    last commit

---
+++ Temeller/depolamaıkı/app.js
@@ -0,0 +1,34 @@
+// local storge 
+
+// setitem
+
+// localStorage.setItem("hareket", "burpe");
+// localStorage.setItem("tekrar", 50 );
+
+// getıtem
+
+// const value = localStorage.getItem("tekrar");
+// console.log(value);
+// console.log(typeof value);
+
+//clear local storage 
+
+// localStorage.setItem("hareket", "burpe");
+// localStorage.setItem("tekrar", 50 );
+
+console.log(localStorage.getItem("tekrar"));
+
+// if(localStorage.getItem("sport") == null ){
+
+// console.log("sorguladığınız veri yok");
+
+// } 
+
+// else  {
+
+// console.log("sorguladığınız veri bulunuyor ");
+
+// }
+
+
+// local storage -- array yazma 
